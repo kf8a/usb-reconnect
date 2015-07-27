98faa9c8f00f0d5c4e9ed84994d23f2ff4e90305
Sven Bohm <bohms@msu.edu>
2015-07-27 12:48:13 -0400

    try to re-connect usb when disconnect is detected

--- main.go
+++ main.go
@@ -1,18 +1,27 @@
 package main
 
 import (
-	"fmt"
 	"github.com/ActiveState/tail"
+	"log"
+	"os/exec"
 	"regexp"
 )
 
 func main() {
 	var usb_disconnect = regexp.MustCompile("usb 1-2: reset full-speed USB device number 15")
+	var usb_reconnect = "attach-device peaksimple /home/bohms/usb.xml"
 
-	t, _ := tail.TailFile("/var/log/syslog", tail.Config{Follow: true})
+	t, err := tail.TailFile("/var/log/syslog", tail.Config{Follow: true})
+	if err != nil {
+		log.Fatal(err)
+	}
 	for line := range t.Lines {
 		if usb_disconnect.Match([]byte(line.Text)) {
-			fmt.Println(line.Text)
+			log.Println(line.Text)
+			_, err := exec.Command("virsh", usb_reconnect).Output()
+			if err != nil {
+				log.Fatal(err)
+			}
 		}
 	}
 }
