2d5609ac86752c08beb72ab4ca1e8b51e1ab242a
Sven Bohm <bohms@msu.edu>
2015-07-27 12:39:36 -0400

    look only at usb messages

--- main.go
+++ main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"fmt"
 	"github.com/ActiveState/tail"
+	"regexp"
 )
 
 func main() {
-	t, _ := tail.TailFile("/var/log/nginx.log", tail.Config{Follow: true})
+	var usb_disconnect = regexp.MustCompile("usb 1-2: reset full-speed USB device number 15")
+
+	t, _ := tail.TailFile("/var/log/syslog", tail.Config{Follow: true})
 	for line := range t.Lines {
-		fmt.Println(line.Text)
+		if usb_disconnect.Match([]byte(line.Text)) {
+			fmt.Println(line.Text)
+		}
 	}
 }
