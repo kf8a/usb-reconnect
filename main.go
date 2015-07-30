2aa8eb9ea04c43a5147474dba87ad2d1769c5416
Sven Bohm <bohms@msu.edu>
2015-07-30 09:20:10 -0400

    log all the lines for now

--- main.go
+++ main.go
@@ -5,22 +5,27 @@ import (
 	"log"
 	"os/exec"
 	"regexp"
+	"time"
 )
 
 func main() {
 	var usb_disconnect = regexp.MustCompile("usb 1-2: reset full-speed USB device number 15")
 	var usb_reconnect = "attach-device peaksimple /home/bohms/usb.xml"
 
-	t, err := tail.TailFile("/var/log/syslog", tail.Config{Follow: true})
+	t, err := tail.TailFile("/var/log/syslog", tail.Config{
+		Follow: true,
+		ReOpen: true,
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
 	for line := range t.Lines {
+		log.Println(line.Text)
 		if usb_disconnect.Match([]byte(line.Text)) {
 			log.Println(line.Text)
 			_, err := exec.Command("virsh", usb_reconnect).Output()
 			if err != nil {
-				log.Fatal(err)
+				log.Println(err)
 			}
 		}
 	}
