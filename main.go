410c6370d310696e06e495721a81721f1c6b58e3
Sven Bohm <bohms@msu.edu>
2015-07-31 08:17:46 -0400

    only show usb related messages

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 
 func main() {
 	var usb_disconnect = regexp.MustCompile("usb 1-2: reset full-speed USB device number 15")
+	var usb_message = regexp.MustCompile("usb")
 	var usb_reconnect = "attach-device peaksimple /home/bohms/usb.xml"
 
 	t, err := tail.TailFile("/var/log/syslog", tail.Config{
@@ -19,7 +20,9 @@ func main() {
 		log.Fatal(err)
 	}
 	for line := range t.Lines {
-		log.Println(line.Text)
+		if usb_message.Match([]byte(line.Text)) {
+			log.Println(line.Text)
+		}
 		if usb_disconnect.Match([]byte(line.Text)) {
 			log.Println(line.Text)
 			_, err := exec.Command("virsh", usb_reconnect).Output()
