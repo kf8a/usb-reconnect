b47abd6789a8d59d47f3c71ed7b47eb25c837c79
Sven Bohm <bohms@msu.edu>
2015-08-03 09:28:47 -0400

    reconnect on usb new and disconnect

--- main.go
+++ main.go
@@ -8,7 +8,8 @@ import (
 )
 
 func main() {
-	var usb_disconnect = regexp.MustCompile("usb 1-2: New USB device found, idVendor=0724, idProduct=0004")
+	var usb_found = regexp.MustCompile("usb 1-2: New USB device found, idVendor=0724, idProduct=0004")
+	var usb_disconnect = regexp.MustCompile("usb 1-2: reset full-speed USB device")
 	var usb_message = regexp.MustCompile("usb")
 	var usb_reconnect = "attach-device peaksimple /home/bohms/usb.xml"
 
@@ -23,7 +24,8 @@ func main() {
 		if usb_message.Match([]byte(line.Text)) {
 			log.Println(line.Text)
 		}
-		if usb_disconnect.Match([]byte(line.Text)) {
+		if usb_disconnect.Match([]byte(line.Text)) || usb_found.Match([]byte(line.Text)) {
+			log.Println("usb disconnect detected ... reconnecting")
 			log.Println(line.Text)
 			_, err := exec.Command("virsh", usb_reconnect).Output()
 			if err != nil {
