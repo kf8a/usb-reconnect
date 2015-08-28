3cfb632b82886c2b4c1e660f34b27e4fd5accd48
Sven Bohm <bohms@msu.edu>
2015-08-28 12:51:59 -0400

    split the two errors into two events and add a timeout to not trigger everything again when restarting the service

--- main.go
+++ main.go
@@ -2,13 +2,32 @@ package main
 
 import (
 	"github.com/ActiveState/tail"
+	"github.com/prometheus/client_golang/prometheus"
 	"log"
+	"net/http"
 	"os/exec"
 	"regexp"
 	"time"
 )
 
-func main() {
+var (
+	usbFailures = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "usb_errors_total",
+		Help: "Number of usb errors.",
+	})
+	usbNewEvents = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: "usb_new_total",
+		Help: "Number of new usb devices found.",
+	})
+)
+
+func init() {
+	prometheus.MustRegister(usbFailures)
+	prometheus.MustRegister(usbNewEvents)
+}
+
+func findUsbErrors() {
+	var rundelayTime = time.Now().Add(1 * time.Minute)
 	var usb_found = regexp.MustCompile("usb 1-2: New USB device found, idVendor=0724, idProduct=0004")
 	var usb_disconnect = regexp.MustCompile("usb 1-2: reset full-speed USB device")
 	var usb_message = regexp.MustCompile("usb")
@@ -22,17 +41,33 @@ func main() {
 		log.Fatal(err)
 	}
 	for line := range t.Lines {
-		if usb_message.Match([]byte(line.Text)) {
-			log.Println(line.Text)
-		}
-		if usb_disconnect.Match([]byte(line.Text)) || usb_found.Match([]byte(line.Text)) {
-			log.Println("usb disconnect detected ... reconnecting")
-			time.Sleep(600)
-			output, err := exec.Command("virsh", usb_reconnect).Output()
-			log.Println(string(output))
-			if err != nil {
-				log.Println(err)
+		if time.Now().After(rundelayTime) {
+			if usb_message.Match([]byte(line.Text)) {
+				log.Println(line.Text)
+			}
+			if usb_disconnect.Match([]byte(line.Text)) {
+				usbFailures.Inc()
+				log.Println("usb disconnected ...")
+			}
+			if usb_found.Match([]byte(line.Text)) {
+				usbNewEvents.Inc()
+				log.Println("usb disconnect detected ... reconnecting")
+				time.Sleep(600)
+				output, err := exec.Command("virsh", usb_reconnect).Output()
+				log.Println(string(output))
+				if err != nil {
+					log.Println(err)
+				}
 			}
 		}
 	}
+}
+
+func main() {
+
+	go findUsbErrors()
+
+	http.Handle("/metrics", prometheus.Handler())
+	http.ListenAndServe(":9101", nil)
+
 }
